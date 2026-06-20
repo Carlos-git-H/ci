d10fa6efbe505b38dbe74d7b375b2a14731a376a
carloshv <jhuaccachi@gmail.com>
2026-06-20 13:31:55 -0500

    first commit

---
+++ src/main/java/com/prueba_ci/PruebaCiApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prueba_ci/PruebaCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
